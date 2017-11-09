046f89fb585aa452ccc6c397f712221ddbd10aa2
Robin <robin.white280@gmail.com>
2017-11-09 14:46:01 +0000

    added french days

--- days.js
+++ days.js
@@ -1,13 +1,13 @@
 (function(){
   var date = new Date();
   var days = [
-    ["Воскресенье","voskresen'ye/sonntag","星期日/xīng qí rì"], //sunday
+    ["Воскресенье","lundi/sonntag","星期日/xīng qí rì"],     //sunday
     ["Понедельник","ponedel'nik/montag","星期一/xīng qí yī"],   //monday
-    ["Вторник","vtornik/dienstag","星期二/xīng qí'èr"],         //tuesday
-    ["Среда","sreda/mittwoch","星期三/xīng qí sān"],            //wednesday
-    ["Четверг","chetverk/donnerstag","星期四/xīng qí sì"],      //thursday
-    ["Пятница","pyatnitsa/freitag","星期五/xīng qí wǔ"],        //friday
-    ["Суббота","subbota/samstag","星期六/xīng qí liù"]          //saturday
+    ["Вторник","mardi/dienstag","星期二/xīng qí'èr"],         //tuesday
+    ["Среда","mercredi/mittwoch","星期三/xīng qí sān"],            //wednesday
+    ["Четверг","jeudi/donnerstag","星期四/xīng qí sì"],      //thursday
+    ["Пятница","vendredi/freitag","星期五/xīng qí wǔ"],        //friday
+    ["Суббота","samedi/samstag","星期六/xīng qí liù"]          //saturday
   ];
   document.getElementById('russian').innerHTML = days[date.getDay()][0];
   document.getElementById('german').innerHTML  = days[date.getDay()][1];
